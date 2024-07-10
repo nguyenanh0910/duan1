d9b1f9d105f7f559306c014e031abcdbf9c41620
nguyenanh0910 <nguyenhongphianh.09@gmail.com>
2024-07-11 05:52:52 +0700

    db moi

--- duan1_nhom8.sql
+++ duan1_nhom8.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 10, 2024 at 06:16 PM
+-- Generation Time: Jul 10, 2024 at 10:52 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -36,13 +36,41 @@ CREATE TABLE `tb_anhsanpham` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tb_chucvu`
+--
+
+CREATE TABLE `tb_chucvu` (
+  `id_chuc_vu` int NOT NULL,
+  `ten_chuc_vu` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tb_chucvu`
+--
+
+INSERT INTO `tb_chucvu` (`id_chuc_vu`, `ten_chuc_vu`) VALUES
+(1, 'Trưởng phòng'),
+(2, 'Nhân viên');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_danhmuc`
 --
 
 CREATE TABLE `tb_danhmuc` (
   `id_danh_muc` int NOT NULL,
-  `ten_danh_muc` varchar(255) NOT NULL
+  `ten_danh_muc` varchar(255) NOT NULL,
+  `mo_ta` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tb_danhmuc`
+--
+
+INSERT INTO `tb_danhmuc` (`id_danh_muc`, `ten_danh_muc`, `mo_ta`) VALUES
+(1, 'Điện thoại', 'Điện thoại các hãng '),
+(2, 'Laptop', 'Laptop các hãng');
 
 -- --------------------------------------------------------
 
@@ -63,6 +91,13 @@ CREATE TABLE `tb_sanpham` (
   `trang_thai` tinyint NOT NULL COMMENT '1 : còn hàng / 0 : hết hàng'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `tb_sanpham`
+--
+
+INSERT INTO `tb_sanpham` (`id_san_pham`, `ten_san_pham`, `gia_san_pham`, `gia_khuyen_mai`, `hinh_anh`, `so_luong`, `ngay_nhap`, `mo_ta`, `id_danh_muc`, `trang_thai`) VALUES
+(1, 'dggd', 10000.00, 100000.00, '1', 10, '2024-07-10', 'okok', 1, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -71,16 +106,25 @@ CREATE TABLE `tb_sanpham` (
 
 CREATE TABLE `tb_taikhoan` (
   `id_tai_khoan` int NOT NULL,
-  `ho_ten` varchar(255) NOT NULL,
+  `ho_ten` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `anh_dai_dien` varchar(255) NOT NULL,
-  `ten_dang_nhap` varchar(50) NOT NULL,
+  `ten_dang_nhap` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `mat_khau` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `so_dien_thoai` varchar(15) NOT NULL,
-  `dia_chi` varchar(255) NOT NULL,
-  `id_chuc_vu` int NOT NULL,
+  `dia_chi` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id_chuc_vu` int DEFAULT NULL,
   `vai_tro` tinyint NOT NULL COMMENT '0: client / 1: admin'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tb_taikhoan`
+--
+
+INSERT INTO `tb_taikhoan` (`id_tai_khoan`, `ho_ten`, `anh_dai_dien`, `ten_dang_nhap`, `mat_khau`, `email`, `so_dien_thoai`, `dia_chi`, `id_chuc_vu`, `vai_tro`) VALUES
+(3, 'nguyễn văn a', 'dfds', 'a', '12323', '1314ee', '09765', 'ư2233', 1, 1),
+(4, 'nguyễn văn a', 'dfds', 'a', '12323', '1314ee', '09765', 'ư2233', 2, 1),
+(5, '21232', '333232', '3333', '3322', '323232', '33433', '32324343', NULL, 0);
 
 --
 -- Indexes for dumped tables
@@ -93,6 +137,12 @@ ALTER TABLE `tb_anhsanpham`
   ADD PRIMARY KEY (`id_anh_san_pham`);
 
 --
+-- Indexes for table `tb_chucvu`
+--
+ALTER TABLE `tb_chucvu`
+  ADD PRIMARY KEY (`id_chuc_vu`);
+
+--
 -- Indexes for table `tb_danhmuc`
 --
 ALTER TABLE `tb_danhmuc`
@@ -102,13 +152,15 @@ ALTER TABLE `tb_danhmuc`
 -- Indexes for table `tb_sanpham`
 --
 ALTER TABLE `tb_sanpham`
-  ADD PRIMARY KEY (`id_san_pham`);
+  ADD PRIMARY KEY (`id_san_pham`),
+  ADD KEY `lk_dm` (`id_danh_muc`);
 
 --
 -- Indexes for table `tb_taikhoan`
 --
 ALTER TABLE `tb_taikhoan`
-  ADD PRIMARY KEY (`id_tai_khoan`);
+  ADD PRIMARY KEY (`id_tai_khoan`),
+  ADD KEY `lk_chucvu` (`id_chuc_vu`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -121,22 +173,44 @@ ALTER TABLE `tb_anhsanpham`
   MODIFY `id_anh_san_pham` int NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `tb_chucvu`
+--
+ALTER TABLE `tb_chucvu`
+  MODIFY `id_chuc_vu` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `tb_danhmuc`
 --
 ALTER TABLE `tb_danhmuc`
-  MODIFY `id_danh_muc` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_danh_muc` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tb_sanpham`
 --
 ALTER TABLE `tb_sanpham`
-  MODIFY `id_san_pham` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_san_pham` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tb_taikhoan`
 --
 ALTER TABLE `tb_taikhoan`
-  MODIFY `id_tai_khoan` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_tai_khoan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_sanpham`
+--
+ALTER TABLE `tb_sanpham`
+  ADD CONSTRAINT `lk_dm` FOREIGN KEY (`id_danh_muc`) REFERENCES `tb_danhmuc` (`id_danh_muc`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `tb_taikhoan`
+--
+ALTER TABLE `tb_taikhoan`
+  ADD CONSTRAINT `lk_chucvu` FOREIGN KEY (`id_chuc_vu`) REFERENCES `tb_chucvu` (`id_chuc_vu`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
